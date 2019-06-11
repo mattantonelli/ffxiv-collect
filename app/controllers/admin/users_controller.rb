a3dab968165e41382d43e6c5be1da19e48b6f135
Matt Antonelli <contact@raelys.com>
2019-06-11 21:16:01 +0000

    Add selected character to users table
    Removes IP which wasn't very useful and replaces it with the user's
    currently selected character, including a link to its profile.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersController < AdminController
   def index
     @q = User.all.ransack(params[:q])
-    @users = @q.result.order(:created_at).paginate(page: params[:page])
+    @users = @q.result.includes(:character).order(:created_at).paginate(page: params[:page])
   end
 end
