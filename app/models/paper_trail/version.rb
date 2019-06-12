77e7f4e7a22f9b5ba9445d037fb1b7654c4acfe0
Matt Antonelli <contact@raelys.com>
2019-06-12 00:04:18 +0000

    Make Version user optional
    User is optional, since changes can be made from the console or through
    tasks. These changes will be represented in the changelog as "System".

--- app/models/paper_trail/version.rb
+++ app/models/paper_trail/version.rb
@@ -1,6 +1,6 @@
 module PaperTrail
   class Version < ActiveRecord::Base
     include PaperTrail::VersionConcern
-    belongs_to :user, foreign_key: :whodunnit
+    belongs_to :user, foreign_key: :whodunnit, required: false
   end
 end
