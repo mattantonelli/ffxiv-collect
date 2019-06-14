61fb359fcbf4fc76054630070758eae4e1de04cc
Matt Antonelli <contact@raelys.com>
2019-06-14 18:04:35 +0000

    Mod page fixes

--- app/controllers/mod/sources_controller.rb
+++ app/controllers/mod/sources_controller.rb
@@ -1,9 +1,9 @@
 class Mod::SourcesController < ModController
   def destroy
     if Source.find(params[:id]).destroy
-      flash[:success] = 'The mount has been updated.'
+      flash[:success] = 'The source has been deleted.'
     else
-      flash[:error] = 'There was a problem updating the mount.'
+      flash[:error] = 'There was a problem deleting the source.'
     end
 
     redirect_back(fallback_location: root_path)
