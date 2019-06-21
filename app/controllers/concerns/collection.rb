6112ca84c155d285ebe01b9bd7a2a29e8c85ac05
Matt Antonelli <contact@raelys.com>
2019-06-21 11:02:10 +0000

    Open Lodestone settings page in a new tab

--- app/controllers/concerns/collection.rb
+++ app/controllers/concerns/collection.rb
@@ -35,7 +35,7 @@ module Collection
 
     if @character.achievements_count == -1
       flash.now[:alert] = 'Achievements for this character are set to private. You can make your achievements public ' \
-        "#{view_context.link_to('here', 'https://na.finalfantasyxiv.com/lodestone/my/setting/account/')}."
+        "#{view_context.link_to('here', 'https://na.finalfantasyxiv.com/lodestone/my/setting/account/', target: '_blank')}."
     elsif @character.achievements_count == 0
       flash.now[:alert] = 'Achievements are still being loaded for this character. ' \
         "Please #{view_context.link_to('Refresh', refresh_character_path, method: :post)} your character or check back later."
