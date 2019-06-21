597edafa9d71fa49e6c3499bf24c5b3bdcce4f30
Matt Antonelli <contact@raelys.com>
2019-06-21 21:29:44 +0000

    Title improvements
    Adds a progress bar to Titles. Adds the limited time filter to Titles.
    When selected, it will filter out Legacy and Seasonal Event titles.

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -5,6 +5,12 @@ class TitlesController < ApplicationController
 
   def index
     @titles = Title.includes(achievement: { category: :type }).all.order('achievements.patch desc', order: :desc)
+
+    if cookies[:limited] == 'hide'
+      @titles = @titles.where('achievement_categories.type_id <> ?', AchievementType.find_by(name_en: 'Legacy').id)
+        .where('achievements.category_id not in (?)', AchievementCategory.where(name_en: 'Seasonal Events').pluck(:id))
+    end
+
     @achievement_ids = @character&.achievement_ids || []
     @owned = Redis.current.hgetall(:achievements)
   end
