edca8eb3e66020fbd097d2dbd14e1a8d74284525
Matt Antonelli <contact@raelys.com>
2020-01-27 22:21:38 +0000

    Fix duplicate spell aspects on search
    Spell aspects can experience strange duplications due to localization
    issues. Resolves this duplication by searching by the aspect name rather
    than its ID.

--- app/controllers/spells_controller.rb
+++ app/controllers/spells_controller.rb
@@ -4,6 +4,7 @@ class SpellsController < ApplicationController
   def index
     @q = Spell.ransack(params[:q])
     @spells = @q.result.includes(:type, :aspect).include_sources.with_filters(cookies).order(:order).distinct
+    @aspects = SpellAspect.all.order("name_#{I18n.locale}").pluck("name_#{I18n.locale}").uniq
   end
 
   def show
