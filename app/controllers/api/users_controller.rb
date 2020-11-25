74a6820eaf0f4680fe6f553029fc84945b56c8d1
Matt Antonelli <contact@raelys.com>
2020-11-25 17:46:47 +0000

    Add users endpoint to API
    Provides a handy redirect to a Discord user's currently selected
    character using their Discord ID.

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,19 @@
+class Api::UsersController < ApiController
+  def show
+    user = User.find_by(uid: params[:id])
+
+    if user.present?
+      character = user.character
+
+      if !character.present?
+        render json: { status: 404, error: 'User has no character selected' }, status: :not_found
+      elsif !character.public?
+        render json: { status: 403, error: "User's character is set to private" }, status: :forbidden
+      else
+        redirect_to api_character_path(character)
+      end
+    else
+      render json: { status: 404, error: 'User not found' }, status: :not_found
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -84,6 +84,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :characters, only: :show
+    resources :users, only: :show
 
     %i(achievements titles mounts minions orchestrions emotes bardings hairstyles armoires spells fashions).each do |resource|
       resources resource, only: [:index, :show]
