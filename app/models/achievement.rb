f4f527090e549fb55ffce7719e2fe11a295fc67e
Matt Antonelli <contact@raelys.com>
2021-06-20 22:13:44 +0000

    Add Diadem achievement to Time Limited

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -55,7 +55,7 @@ class Achievement < ApplicationRecord
      (2110..2114).to_a + # Feast Championships
      [2487, 2488, 2712, 2713, 2785, 2786] + # Ishgardian Reconstruction
      [1419, 1420, 1421, 1422, 1423, 1424, 1425, 1426, 1427, 1428, 1429, 1430, 1431, 1432,
-      1734, 1735, 1736, 1737, 1743, 1744, 1745]).freeze # Diadem
+      1734, 1735, 1736, 1737, 1743, 1744, 1745, 1746]).freeze # Diadem
   end
 
   private
