9172439473fb804b4a09c3f56e7d3774bd9ae05d
Matt Antonelli <contact@raelys.com>
2021-08-17 02:29:18 +0000

    P2W tweaks
    Adds order to match Mounts page. Bolds table totals.

--- app/controllers/p2w_controller.rb
+++ app/controllers/p2w_controller.rb
@@ -1,7 +1,7 @@
 class P2wController < ApplicationController
   def index
     @prices = JSON.parse(Redis.current.get('p2w-mounts'))
-    @mounts = Mount.where(id: @prices.keys)
+    @mounts = Mount.where(id: @prices.keys).order(patch: :desc, order: :desc)
     @owned = Redis.current.hgetall('mounts')
   end
 end
