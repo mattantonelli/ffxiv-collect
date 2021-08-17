1f7b62d4db6da05a24d6aca6029b34bf16460c1f
Matt Antonelli <contact@raelys.com>
2021-08-17 02:48:21 +0000

    More P2W refactoring
    Adds the mount collectable data to the object we read from Redis,
    reducing the number of lookups. Adds data values to our td's to achieve
    proper sorting.

--- app/controllers/p2w_controller.rb
+++ app/controllers/p2w_controller.rb
@@ -1,7 +1,11 @@
 class P2wController < ApplicationController
   def index
-    @prices = JSON.parse(Redis.current.get('p2w-mounts'))
-    @mounts = Mount.where(id: @prices.keys).order(patch: :desc, order: :desc)
+    @mounts = JSON.parse(Redis.current.get('p2w-mounts'))
+    @characters = Character.visible.recent.count
     @owned = Redis.current.hgetall('mounts')
+
+    Mount.where(id: @mounts.keys).order(patch: :desc, order: :desc).each do |mount|
+      @mounts[mount.id.to_s]['collectable'] = mount
+    end
   end
 end
