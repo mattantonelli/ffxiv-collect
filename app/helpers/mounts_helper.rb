023b466e987f8cd95cbd98e1ef905311bc6e23e2
Matt Antonelli <contact@raelys.com>
2021-09-30 14:02:02 +0000

    Add custom music indicator to Mounts table

--- app/helpers/mounts_helper.rb
+++ app/helpers/mounts_helper.rb
@@ -1,2 +1,8 @@
 module MountsHelper
+  def custom_music_link(mount)
+    if mount.bgm_sample.present?
+      link_to(fa_icon('music'), mount_path(mount), title: I18n.t('mounts.custom_music'),
+              data: { toggle: 'tooltip', html: true } )
+    end
+  end
 end
