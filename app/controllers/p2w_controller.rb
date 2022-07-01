87bd8598b30bec16d5e8b975c686aeeadd236fe0
Matt Antonelli <contact@raelys.com>
2022-07-01 14:01:29 +0000

    Refactor P2W mounts
    Iterates over collectable mounts and grabs the P2W data as needed
    instead of injecting the collectable into the P2W data. Resolves the
    issue where mounts weren't actually being sorted.

--- app/controllers/p2w_controller.rb
+++ app/controllers/p2w_controller.rb
@@ -1,11 +1,8 @@
 class P2wController < ApplicationController
   def index
-    @mounts = JSON.parse(Redis.current.get('p2w-mounts'))
+    @data = JSON.parse(Redis.current.get('p2w-mounts'))
+    @mounts = Mount.where(id: @data.keys).order(patch: :desc, order: :desc)
     @characters = Character.visible.recent.count
     @owned = Redis.current.hgetall('mounts')
-
-    Mount.where(id: @mounts.keys).order(patch: :desc, order: :desc).each do |mount|
-      @mounts[mount.id.to_s]['collectable'] = mount
-    end
   end
 end
