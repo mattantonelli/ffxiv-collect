70a58719e4b249802cf45a29b7dc04e4843499f1
Matt Antonelli <contact@raelys.com>
2022-09-02 21:34:41 +0000

    Remove redundant helper method

--- app/helpers/free_companies_helper.rb
+++ app/helpers/free_companies_helper.rb
@@ -1,8 +1,4 @@
 module FreeCompaniesHelper
-  def free_company_refreshable?(free_company)
-    @free_company.syncable? && !@free_company.in_queue?
-  end
-
   def free_company_collectable_td(character, collectable)
     owned = @owned_ids[character.id].include?(collectable.id)
     expansion = collectable.patch[0]
