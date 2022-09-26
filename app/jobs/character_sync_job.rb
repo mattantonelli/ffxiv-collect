9fbf0f0b368f9107d0cbc4ef662cb272caaa644d
Matt Antonelli <contact@raelys.com>
2022-09-26 16:39:51 +0000

    Add retries to character fetches on rate limit
    Sleep and retry up to 3 times when a character fetch is rate limited by
    the Lodestone.

--- app/jobs/character_sync_job.rb
+++ app/jobs/character_sync_job.rb
@@ -3,12 +3,24 @@ class CharacterSyncJob < ApplicationJob
   unique :until_executed, on_conflict: :log
 
   def perform(*args)
+    id = args[0]
+    attempts = 0
+
     begin
-      Character.fetch(args[0])
+      Character.fetch(id)
     rescue RestClient::BadGateway, RestClient::ServiceUnavailable
       Sidekiq.logger.info('Lodestone is down for maintenance.')
     rescue RestClient::NotFound
-      Sidekiq.logger.info("Character #{args[0]} is no longer available.")
+      Sidekiq.logger.info("Character #{id} is no longer available.")
+    rescue RestClient::TooManyRequests
+      if attempts < 3
+        Sidekiq.logger.info("Rate limited while fetching character #{id}. Retrying...")
+        attempts += 1
+        sleep(3)
+        retry
+      else
+        Sidekiq.logger.error("Rate limited while fetching character #{id}")
+      end
     end
   end
 end
--- app/jobs/free_company_sync_job.rb
+++ app/jobs/free_company_sync_job.rb
@@ -19,12 +19,21 @@ class FreeCompanySyncJob < ApplicationJob
 
         if character.nil? || character.syncable?
           Sidekiq.logger.info('Fetching...')
+          attempts = 0
+
           begin
             Character.fetch(id)
           rescue RestClient::NotFound
             Sidekiq.logger.info("Character #{id} is no longer available.")
           rescue RestClient::TooManyRequests
-            Sidekiq.logger.error("Rate limited while fetching character #{id}")
+            if attempts < 3
+              Sidekiq.logger.info("Rate limited while fetching character #{id}. Retrying...")
+              attempts += 1
+              sleep(3)
+              retry
+            else
+              Sidekiq.logger.error("Rate limited while fetching character #{id}")
+            end
           end
         else
           Sidekiq.logger.info('Character is up to date.')
