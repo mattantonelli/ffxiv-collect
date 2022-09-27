fec46204b73589ae2b73a5c1078b8c827a19aa4c
Matt Antonelli <contact@raelys.com>
2022-09-27 18:25:50 +0000

    Fix Title ownership % in API

--- app/controllers/api/titles_controller.rb
+++ app/controllers/api/titles_controller.rb
@@ -1,11 +1,15 @@
 class Api::TitlesController < ApiController
+  skip_before_action :set_owned
+
   def index
     query = Title.include_related.all.ransack(@query)
     @titles = query.result.ordered.distinct.limit(params[:limit])
+    @owned = Redis.current.hgetall('achievements')
   end
 
   def show
     @title = Title.find_by(id: params[:id])
+    @owned = { @title.achievement_id.to_s => Redis.current.hget('achievements', params[:id]) }
     render_not_found unless @title.present?
   end
 end
