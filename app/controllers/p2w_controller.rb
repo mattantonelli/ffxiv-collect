1ccfe97caaaa585adba8207286ecd4a9db14a7d3
Matt Antonelli <contact@raelys.com>
2023-07-24 14:00:46 +0000

    Add the latest P2W mounts

--- app/controllers/p2w_controller.rb
+++ app/controllers/p2w_controller.rb
@@ -1,7 +1,7 @@
 class P2wController < ApplicationController
   def index
     @data = JSON.parse(Redis.current.get('p2w-mounts'))
-    @mounts = Mount.where(id: @data.keys).order(patch: :desc, order: :desc)
+    @mounts = Mount.where(id: @data.keys).ordered
     @characters = Character.visible.recent.count
     @owned = {
       count: Redis.current.hgetall('mounts-count'),
