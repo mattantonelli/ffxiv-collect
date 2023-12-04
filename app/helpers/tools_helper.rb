d137f86fe4ad1e184ecdb01879ac20f9e1fa75f0
Matt Antonelli <contact@raelys.com>
2023-12-04 20:36:19 +0000

    Add safe navigation to generic collectable value
    Ensures we do not have an issue when @values is absent.

--- app/helpers/tools_helper.rb
+++ app/helpers/tools_helper.rb
@@ -1,5 +1,5 @@
 module ToolsHelper
   def generic_collectable_value(collectable)
-    @values[collectable.class.to_s.underscore.pluralize.to_sym][collectable.id] || 0
+    @values&.dig(collectable.class.to_s.underscore.pluralize.to_sym, collectable.id) || 0
   end
 end
