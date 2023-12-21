b07696a72e39b5210c838e4fbc69c42a3727efc7
Matt Antonelli <contact@raelys.com>
2023-12-21 15:43:49 +0000

    Improve staggering of scheduled jobs

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,23 +2,23 @@
 set :bundle_command, '~/.rbenv/shims/bundle exec'
 set :output, 'log/whenever.log'
 
-# Cache collectable ownership
-every '10 3 * * *' do
-  rake 'ownership:cache'
-end
-
 # Cache leaderboard rankings for character profiles and the API
 every '0 0,6,12,18 * * *' do
   rake 'rankings:cache'
 end
 
 # Cache item prices and FAQ stats
-every '0 0 * * *' do
+every '0 1 * * *' do
   rake 'prices:cache'
   rake 'stats:cache'
 end
 
 # Cache P2W data
-every '5 0 * * *' do
+every '10 1 * * *' do
   rake 'p2w:cache'
+end
+
+# Cache collectable ownership
+every '10 3 * * *' do
+  rake 'ownership:cache'
 end
