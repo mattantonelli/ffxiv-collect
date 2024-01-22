43157171cbf7f20c218f73929223064904fe15af
Matt Antonelli <contact@raelys.com>
2024-01-22 19:16:53 +0000

    Add missing comparison IDs to Packs

--- app/controllers/triad/packs_controller.rb
+++ app/controllers/triad/packs_controller.rb
@@ -2,5 +2,6 @@ class Triad::PacksController < ApplicationController
   def index
     @packs = Pack.all.includes(cards: :type)
     @collection_ids = @character&.card_ids || []
+    @comparison_ids = @comparison&.card_ids || []
   end
 end
