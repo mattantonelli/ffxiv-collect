c37de2711caf2d4d80803fa06bc7623aa80a54b3
Matt Antonelli <contact@raelys.com>
2024-11-22 01:49:27 +0000

    Improve deploy config readability

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,8 +18,10 @@ namespace :deploy do
       execute :ln, '-s', shared_path.join('master.key'), release_path.join('config/master.key')
 
       # Persisted logs
-      execute :ln, '-s', shared_path.join("log/#{fetch(:rails_env)}.log"), release_path.join("log/#{fetch(:rails_env)}.log")
-      execute :ln, '-s', shared_path.join('log/whenever.log'), release_path.join('log/whenever.log')
+      execute :ln, '-s', shared_path.join("log/#{fetch(:rails_env)}.log"),
+        release_path.join("log/#{fetch(:rails_env)}.log")
+      execute :ln, '-s', shared_path.join('log/whenever.log'),
+        release_path.join('log/whenever.log')
 
       # Game data
       execute :rmdir, release_path.join('vendor/xiv-data')
@@ -30,7 +32,8 @@ namespace :deploy do
         %w(footprint large small).each do |type|
           path = "#{model}/#{type}"
           execute :rm, '-rf', release_path.join('public/images', path)
-          execute :ln, '-s', shared_path.join('public/images', path), release_path.join('public/images', path)
+          execute :ln, '-s', shared_path.join('public/images', path),
+            release_path.join('public/images', path)
         end
       end
 
@@ -38,24 +41,33 @@ namespace :deploy do
         %w(large small).each do |type|
           path = "#{model}/#{type}"
           execute :rm, '-rf', release_path.join('public/images', path)
-          execute :ln, '-s', shared_path.join('public/images', path), release_path.join('public/images', path)
+          execute :ln, '-s', shared_path.join('public/images', path),
+            release_path.join('public/images', path)
         end
       end
 
       %w(achievements armoires bardings emotes relics hairstyles facewear spells achievement_items leve_items frames).each do |model|
         execute :rm, '-rf', release_path.join('public/images', model)
-        execute :ln, '-s', shared_path.join('public/images', model), release_path.join('public/images', model)
+        execute :ln, '-s', shared_path.join('public/images', model),
+          release_path.join('public/images', model)
       end
 
       # Triple Triad card images
       execute :rm, '-rf', release_path.join('public/images/cards/large')
-      execute :ln, '-s', shared_path.join('public/images/cards/large'), release_path.join('public/images/cards/large')
+      execute :ln, '-s', shared_path.join('public/images/cards/large'),
+        release_path.join('public/images/cards/large')
+
       execute :rm, '-rf', release_path.join('public/images/cards/small')
-      execute :ln, '-s', shared_path.join('public/images/cards/small'), release_path.join('public/images/cards/small')
+      execute :ln, '-s', shared_path.join('public/images/cards/small'),
+        release_path.join('public/images/cards/small')
+
       execute :rm, '-rf', release_path.join('public/images/cards/blue')
-      execute :ln, '-s', shared_path.join('public/images/cards/blue'), release_path.join('public/images/cards/blue')
+      execute :ln, '-s', shared_path.join('public/images/cards/blue'),
+        release_path.join('public/images/cards/blue')
+
       execute :rm, '-rf', release_path.join('public/images/cards/red')
-      execute :ln, '-s', shared_path.join('public/images/cards/red'), release_path.join('public/images/cards/red')
+      execute :ln, '-s', shared_path.join('public/images/cards/red'),
+        release_path.join('public/images/cards/red')
 
       # Music samples
       execute :rm, '-rf', release_path.join('public/music')
@@ -77,32 +89,52 @@ namespace :deploy do
     on roles(:app) do
       # Copy the spritesheets from the previous deployment if they exist. Otherwise, generate them.
       if test("[ -f #{current_path.join('app/assets/images/mounts-small.png')} ]")
-        execute :cp, current_path.join('app/assets/images/mounts-small.png'), release_path.join('app/assets/images')
-        execute :cp, current_path.join('app/assets/images/minions-small.png'), release_path.join('app/assets/images')
-        execute :cp, current_path.join('app/assets/images/achievements.png'), release_path.join('app/assets/images')
-        execute :cp, current_path.join('app/assets/images/achievement_items.png'), release_path.join('app/assets/images')
-        execute :cp, current_path.join('app/assets/images/emotes.png'), release_path.join('app/assets/images')
-        execute :cp, current_path.join('app/assets/images/bardings.png'), release_path.join('app/assets/images')
-        execute :cp, current_path.join('app/assets/images/armoires.png'), release_path.join('app/assets/images')
+        execute :cp, current_path.join('app/assets/images/mounts-small.png'),
+          release_path.join('app/assets/images')
+        execute :cp, current_path.join('app/assets/images/minions-small.png'),
+          release_path.join('app/assets/images')
+        execute :cp, current_path.join('app/assets/images/achievements.png'),
+          release_path.join('app/assets/images')
+        execute :cp, current_path.join('app/assets/images/achievement_items.png'),
+          release_path.join('app/assets/images')
+        execute :cp, current_path.join('app/assets/images/emotes.png'),
+          release_path.join('app/assets/images')
+        execute :cp, current_path.join('app/assets/images/bardings.png'),
+          release_path.join('app/assets/images')
+        execute :cp, current_path.join('app/assets/images/armoires.png'),
+          release_path.join('app/assets/images')
         execute :cp, '-R', current_path.join('app/assets/images/hairstyles/*.png'),
           release_path.join('app/assets/images/hairstyles')
         execute :cp, '-R', current_path.join('app/assets/images/facewear/*.png'),
           release_path.join('app/assets/images/facewear')
-        execute :cp, current_path.join('app/assets/images/spells.png'), release_path.join('app/assets/images')
-        execute :cp, current_path.join('app/assets/images/relics-*.png'), release_path.join('app/assets/images')
-        execute :cp, current_path.join('app/assets/images/fashions-small.png'), release_path.join('app/assets/images')
-        execute :cp, current_path.join('app/assets/images/records-small.png'), release_path.join('app/assets/images')
-        execute :cp, current_path.join('app/assets/images/survey_records-small.png'), release_path.join('app/assets/images')
-        execute :cp, current_path.join('app/assets/images/cards-large.png'), release_path.join('app/assets/images')
-        execute :cp, current_path.join('app/assets/images/cards-small.png'), release_path.join('app/assets/images')
-        execute :cp, current_path.join('app/assets/images/leve_items.png'), release_path.join('app/assets/images')
-        execute :cp, current_path.join('app/assets/stylesheets/images/*.scss'), release_path.join('app/assets/stylesheets/images')
+        execute :cp, current_path.join('app/assets/images/spells.png'),
+          release_path.join('app/assets/images')
+        execute :cp, current_path.join('app/assets/images/relics-*.png'),
+          release_path.join('app/assets/images')
+        execute :cp, current_path.join('app/assets/images/fashions-small.png'),
+          release_path.join('app/assets/images')
+        execute :cp, current_path.join('app/assets/images/records-small.png'),
+          release_path.join('app/assets/images')
+        execute :cp, current_path.join('app/assets/images/survey_records-small.png'),
+          release_path.join('app/assets/images')
+        execute :cp, current_path.join('app/assets/images/cards-large.png'),
+          release_path.join('app/assets/images')
+        execute :cp, current_path.join('app/assets/images/cards-small.png'),
+          release_path.join('app/assets/images')
+        execute :cp, current_path.join('app/assets/images/leve_items.png'),
+          release_path.join('app/assets/images')
+        execute :cp, current_path.join('app/assets/stylesheets/images/*.scss'),
+          release_path.join('app/assets/stylesheets/images')
 
         # Horizontal card spritesheets
-        execute :cp, current_path.join('public/images/cards-large.png'), release_path.join('public/images')
-        execute :cp, current_path.join('public/images/cards-red.png'), release_path.join('public/images')
-        execute :cp, current_path.join('public/images/cards-blue.png'), release_path.join('public/images')
-        execute :cp, current_path.join('public/images/cards-small.png'), release_path.join('public/images')
+        execute :cp, current_path.join('public/images/cards-large.png'),
+          release_path.join('public/images')
+        execute :cp, current_path.join('public/images/cards-red.png'),
+          release_path.join('public/images')
+        execute :cp, current_path.join('public/images/cards-blue.png'),
+          release_path.join('public/images')
+        execute :cp, current_path.join('public/images/cards-small.png'),
+          release_path.join('public/images')
       else
         within release_path do
           with rails_env: fetch(:rails_env) do
