aec0898f008dacfcd559d1ef560266bb838c0f7e
Matt Antonelli <contact@raelys.com>
2025-02-02 19:33:11 +0000

    Add owned/missing API support for TT collections

--- app/controllers/api/characters_controller.rb
+++ app/controllers/api/characters_controller.rb
@@ -28,6 +28,11 @@ class Api::CharactersController < ApiController
 
   def set_collection
     @collection = params[:collection]
+    @partial = "api/#{@collection}/#{@collection.singularize}"
+
+    if @collection.match?(/cards|npcs/)
+      @partial.sub!(/^api/, 'api/triad')
+    end
 
     unless view_context.public_collection?(@character, @collection)
       return render json: { status: 403, error: 'Collection is set to private' }, status: :forbidden
--- app/views/api/characters/ownership.json.jbuilder
+++ app/views/api/characters/ownership.json.jbuilder
@@ -1,3 +1,3 @@
 json.cache! [@character, @collectables] do
-  json.partial! "api/#{@collection}/#{@collection.singularize}", collection: @collectables, as: @collection.singularize
+  json.partial! @partial, collection: @collectables, as: @collection.singularize
 end
